be02aea382a9419ee2b1629add98a064a8db5857
Josiah Saunders <contact@josiahsaunders.com>
2022-12-25 00:06:07 -0600

    candy

---
+++ game/Assets/Scripts/BouncyRotator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyRotator : MonoBehaviour
+{
+	public float bounceSpeed = 1f;
+	public float bounceHeight = 1f;
+	public float rotationSpeed = 1f;
+
+	void LateUpdate()
+	{
+		transform.localPosition = new Vector3(0, Mathf.Sin(Time.time * bounceSpeed) * bounceHeight, 0);
+		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
